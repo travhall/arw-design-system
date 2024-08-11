ee13b6825948a2179332474d35bd600751c45979
Travis Hall <unkletas@gmail.com>
2024-08-10 20:46:25 -0500

    Added Tailwind and Logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,39 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Interface Design System",
+      logo: {
+        light: "./src/assets/arrow-logo-light.svg",
+        dark: "./src/assets/arrow-logo-dark.svg",
+        replacesTitle: true,
+      },
+      customCss: ["./src/css/tailwind.css"],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
